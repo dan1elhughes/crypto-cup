7f9183f8d47a862281ded32bfd7cabe528d14bf5
Dan Hughes <dan.hughes@mirumagency.com>
2017-11-02 11:22:30 +0000

    Add sample data and react linechart

--- browser/src/App.js
+++ browser/src/App.js
@@ -1,22 +1,65 @@
 import React from 'react';
 
-export default class Module extends React.Component {
+import LineChart from 'react-linechart';
+import '../node_modules/react-linechart/dist/styles.css';
+
+import toHex from './util/toHex';
+
+export default class App extends React.Component {
 
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			data: [
+				{ name: '', points: [] },
+			],
+		};
 	}
 
 	componentDidMount() {
 		fetch('/api')
 			.then(res => res.json())
-			.then(({ message }) => this.setState({ message }));
+			.then(response => {
+				this.setState({ data: this.parseData(response) });
+			});
+	}
+
+	parseData(data) {
+		return data.map(({ id, points }) => ({
+			name: id,
+			points: points.map(point => {
+				const [ x, y ] = point.split(',').map(_ => parseInt(_, 10));
+				return { x, y };
+			}),
+			color: toHex(id),
+		}));
+	}
+
+	xDisplay(x) {
+		const date = new Date(x * 1000);
+		const hours = date.getHours();
+		const minutes = '0' + date.getMinutes();
+		const seconds = '0' + date.getSeconds();
+
+		const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+		return formattedTime;
 	}
 
 	render() {
 		return (
-			<div className="Module">{this.state.message}</div>
+			<div className="App">
+				<pre>{JSON.stringify(this.state, null, 4)}</pre>
+				<LineChart
+					width={600}
+					height={400}
+					data={this.state.data}
+					showLegends={true}
+					// isDate={true}
+					interpolate={false}
+					xDisplay={this.xDisplay}
+				/>
+			</div>
 		);
 	}
 
--- server/server.js
+++ server/server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const data = require('./data');
+
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../browser/build')));
 
 // Answer API requests.
 app.get('/api', (req, res) => {
 	res.set('Content-Type', 'application/json');
-	res.send({message: 'Hello from the custom server!'});
+	res.send(data);
 });
 
 // All remaining requests return the React app, so it can handle routing.
